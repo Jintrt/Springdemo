35d18b10ea751600fffc2cb13c0731aecfc494e3
Jin <alexanderpolakowski@icloud.com>
2024-12-12 15:35:43 +0100

    test update

--- src/main/java/com/Jin/Springdemo/student/Student.java
+++ src/main/java/com/Jin/Springdemo/student/Student.java
@@ -1,4 +1,68 @@
 package com.Jin.Springdemo.student;
 
+import java.time.LocalDate;
+
 public class Student {
+
+    private String name;
+
+    private String lastname;
+
+    private LocalDate dateofbirth;
+
+    private String email;
+
+    private int age;
+
+    public Student(
+            String name, String lastname, LocalDate dateofbirth, String email, int age) {
+
+        this.name = name;
+        this.lastname = lastname;
+        this.dateofbirth = dateofbirth;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Student() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDate getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(LocalDate dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/main/java/com/Jin/Springdemo/student/StudentControler.java
+++ src/main/java/com/Jin/Springdemo/student/StudentControler.java
@@ -3,7 +3,7 @@ package com.Jin.Springdemo.student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -11,10 +11,10 @@ import java.util.List;
 
 public class StudentControler {
     @GetMapping
-    public List<String> findAllstudents(){
+    public List<Student> findAllstudents(){
         return List.of(
-                "Jin",
-                "Hello World"
+                new Student("Jin", "Polak", LocalDate.of(2005, 12, 1), "Contact@polJin.com", 18),
+                new Student("Jinv2", "Polakv", LocalDate.of(2004, 6, 15), "Contact@polJinv.com", 19)
         );
     }
 }
